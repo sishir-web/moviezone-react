69221c5cdba760a8961409612f777451f0b325e0
sishir-web <sishirdhulabari@gmail.com>
2025-05-06 20:01:26 +0545

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Moviezone from './components/Moviezone.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Moviezone/>
+    </>
+  )
+}
+
+export default App
